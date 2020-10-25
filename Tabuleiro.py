803f3ca0fcfc99aeaa54fe144a88128581faab6a
glaucio <glaucio@mail.com>
2020-10-25 14:57:31 -0300

    Novos ajustes gerais

--- Tabuleiro.py
+++ Tabuleiro.py
@@ -1,20 +1,19 @@
 import pygame
 
-from constantes import *
+from constantes import Cores
+from constantes import Configurations as conf
+
 
 class Tabuleiro:
-
 
     def __init__(self, n):
         self.peca = None
         self.nome = n
 
-
     def iniciarTabuleiro(self, interface):
 
-        interface.fill(preto)
+        interface.fill(Cores.preto)
 
-        for col in range(colunas):
-            for lin in range(col % 2, linhas, 2):
-                pygame.draw.rect(interface, rosaClaro, (col * quadrado, lin * quadrado, quadrado, quadrado))
-
+        for col in range(conf.colunas):
+            for lin in range(col % 2, conf.linhas, 2):
+                pygame.draw.rect(interface, Cores.branco, (col * conf.quadrado, lin * conf.quadrado, conf.quadrado, conf.quadrado))
--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import pygame
-import pygame_menu
 from MenuCustom import MenuCustom
-
-from constantes import *
-
 from Tabuleiro import Tabuleiro
+import pygame_menu
+from constantes import Cores
+from constantes import Configurations as conf
+from constantes import tema
 
 def main():
-
-
     running = True
-    #clock = pygame.time.Clock()
+    # clock = pygame.time.Clock()
 
     tab = Tabuleiro("meuTab")
 
@@ -18,21 +16,21 @@ def main():
 
     pygame.display.update()
     while running:
-        #clock.tick(10)
+        # clock.tick(10)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-            #if event.type == pygame.MOUSEBUTTONDOWN:
-               # pass
-
+            # if event.type == pygame.MOUSEBUTTONDOWN:
+            # pass
 
     pygame.quit()
 
+
 def sobreOJogo():
     running = True
-    gamedisplay.fill(rosaClaro)
+    gamedisplay.fill(Cores.rosaClaro)
     pygame.font.init()
     myfont = pygame.font.SysFont('Comic Sans MS', 30)
     textsurface = myfont.render('Trabalho sobre a materia ES2', False, (0, 0, 0))
@@ -45,17 +43,16 @@ def sobreOJogo():
                 running = False
 
 
-
 pygame.init()
 
-gamedisplay = pygame.display.set_mode((largura, altura))
+gamedisplay = pygame.display.set_mode((conf.largura, conf.altura))
 pygame.display.set_caption("Jogo de Damas")
 
-menu = pygame_menu.Menu(400,400, 'Bem-vindo', theme = tema)
-#menu = MenuCustom(400,400, 'Bem-vindo', theme = tema)
+menu = MenuCustom(conf.largura, conf.altura, 'Bem-vindo', theme=tema)
 
-menu.add_text_input('Name:', default = 'Lucas')
+menu.add_text_input('Name:', default='Lucas')
 menu.add_button('Play', main)
+menu.add_button('Configurações', sobreOJogo)
 menu.add_button('Sobre', sobreOJogo)
 menu.add_button('Quit', pygame_menu.events.EXIT)
 menu.add_image("jogo-damas.jpg", angle=10, scale=(0.15, 0.15))
