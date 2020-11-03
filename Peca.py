4f0a1539ebe399981c6f970d2eff7c440c40d423
unknown <knust.green@gmail.com>
2020-11-02 23:19:05 -0300

    Criacao das pecas posicionadas no tabuleiro pro início da partida

---
+++ Peca.py
@@ -0,0 +1,19 @@
+import pygame
+
+
+class Peca:
+    def __init__(self, lin, col, cor):
+        self.lin = lin
+        self.col = col
+        self.cor = cor
+
+    def definirPosicao(self, quadradoTam):
+        self.x = int(quadradoTam * self.col + quadradoTam / 2)
+        self.y = int(quadradoTam * self.lin + quadradoTam / 2)
+
+    def iniciarPeca(self, interface, quadradoTam):
+        margem = 10
+        pecaTam = int(quadradoTam/2 - margem)
+        pygame.draw.circle(interface, self.cor, (self.x, self.y), pecaTam)
+
+
--- Tabuleiro.py
+++ Tabuleiro.py
@@ -1,10 +1,20 @@
 import pygame
 
+from Peca import Peca
+from constantes import Cores
+from Configurations import Configurations
+
+
 class Tabuleiro:
+
 
     def __init__(self, n):
         self.peca = None
         self.nome = n
+        self.tabuleiro = []
+        #conf = Configurations()
+        #self.posicionarPeca(conf)
+
 
     def iniciarTabuleiro(self, interface, conf):
 
@@ -13,3 +23,32 @@ class Tabuleiro:
         for col in range(conf.colunas):
             for lin in range(col % 2, conf.linhas, 2):
                 pygame.draw.rect(interface, conf.corB, (col * conf.quadrado, lin * conf.quadrado, conf.quadrado, conf.quadrado))
+
+
+
+    def posicionarPeca(self, conf):
+        for lin in range(conf.linhas):
+            self.tabuleiro.append([])
+            for col in range(conf.colunas):
+                if col % 2 == (lin + 1) % 2:
+                    if lin < 3:
+                        self.tabuleiro[lin].append(Peca(lin, col, Cores.vermelho))
+                    elif lin > 4:
+                        self.tabuleiro[lin].append(Peca(lin, col, Cores.azul))
+                    else:
+                        self.tabuleiro[lin].append(0)
+                else:
+                    self.tabuleiro[lin].append(0)
+
+    def desenharPecas(self, interface, conf):
+        self.iniciarTabuleiro(interface, conf)
+        self.posicionarPeca(conf)
+        for lin in range(conf.linhas):
+
+            for col in range(conf.colunas):
+                peca = self.tabuleiro[lin][col]
+                #print(peca)
+                if peca != 0:
+                    quadradoTam = conf.getResolucaoSelecionada()/8
+                    peca.definirPosicao(quadradoTam)
+                    peca.iniciarPeca(interface, quadradoTam)
