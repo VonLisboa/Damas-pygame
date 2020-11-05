74cd43142e81c087757eea6fab30985d04cb9dbb
Leticia Mello <leticiamellocunha9@yahoo.com.br>
2020-11-04 22:08:00 -0300

    Salvando a configuração da cor e da resolução no banco

--- Configurations.py
+++ Configurations.py
@@ -1,5 +1,6 @@
 import pygame
 import sqlite3
+import os
 
 from constantes import Cores
 
@@ -15,17 +16,64 @@ class Configurations:
 
     def __init__(self):
         # adicionar sqlite pra salvar e recarregar configurações
-        self.indexResolucao = 0
-        self.indexCor = 0
+        self.createTableConfiguracao()
+        self.indexCor, self.indexResolucao = self.insertConfiguracao()
         self.corA, self.corB = self.cores[self.indexCor].get(list(self.cores[self.indexCor].keys())[0])
         self.largura = self.altura = self.resolucao[self.indexResolucao]
         self.quadrado = self.largura / 8
+
+
+    def createTableConfiguracao(self):
+        if not (os.path.exists('jogoDamas.db')):
+            conn = sqlite3.connect('jogoDamas.db')
+            cursor = conn.cursor()
+            cursor.execute("""
+            CREATE TABLE configuracao (
+                    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                    cor INTEGER NOT NULL,
+                    resolucao INTEGER NOT NULL
+            );
+            """)
+            print('Tabela criada com sucesso.')
+            cursor.execute('INSERT INTO configuracao (cor, resolucao) VALUES (0, 0)')
+            conn.commit()
+            print('Dados inseridos com sucesso.')
+            conn.close()
+
+    def insertConfiguracao(self):
+        if (os.path.exists('jogoDamas.db')):
+            conn = sqlite3.connect('jogoDamas.db')
+            cursor = conn.cursor()
+            cursor.execute('SELECT cor, resolucao from configuracao')
+            for linha in cursor.fetchall():
+                return linha
+            conn.close
+
+
+    def updateCor(self, indexCor):
+        conn = sqlite3.connect('jogoDamas.db')
+        cursor = conn.cursor()
+        cursor.execute('UPDATE configuracao SET cor =' + str(indexCor) +  ' WHERE id = 1')
+       # cursor.execute('INSERT INTO configuracao (cor, resolucao) VALUES (0, 0)')
+        conn.commit()
+        print('Dados atualizados com sucesso.')
+        conn.close
+
+    def updateResolucao(self, indexResolucao):
+        conn = sqlite3.connect('jogoDamas.db')
+        cursor = conn.cursor()
+        cursor.execute('UPDATE configuracao SET resolucao =' + str(indexResolucao) +  ' WHERE id = 1')
+        conn.commit()
+        print('Dados atualizados com sucesso.')
+        conn.close
+
 
     def setResolucao(self, i):
         self.index = i
         self.largura = self.altura = self.resolucao[i]
         pygame.display.set_mode((self.largura, self.altura))
         self.quadrado = self.largura / 8
+        self.updateResolucao(self.indexResolucao)
         return self.largura, self.altura
 
     def getResolucaoString(self, i):
@@ -50,6 +98,7 @@ class Configurations:
     def setCorTabuleiro(self, i):
         self.indexCor = i
         self.corA, self.corB = self.cores[i].get(list(self.cores[i].keys())[0])
+        self.updateCor(self.indexCor)
 
     def getResolucaoSelecionada(self):
         return self.altura
