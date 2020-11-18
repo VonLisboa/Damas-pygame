283ac7ca030503f0ef4d24c0343a2814fd6cb47f
glaucio <glaucio@mail.com>
2020-11-18 19:49:57 -0300

    unit test cases

---
+++ testes/unidade/jogo_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+from Peca import Peca
+from constantes import Cores
+
+
+class jogo_test(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        pass
+
+    def test_name(self):
+        pass
+
---
+++ testes/unidade/tabuleiro_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+from Peca import Peca
+from constantes import Cores
+
+
+class tabuleiro_test(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        pass
+
+    def test_name(self):
+        pass
+
